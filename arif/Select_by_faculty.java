665ed18510a21215acd8679b7cfc4c98ce6b614f
Mohamadarif <butterflyhunter84@gmail.com>
2015-09-19 07:14:21 +0800

    Select by faculty from lhr_signs

--- arif/Select_by_faculty.java
+++ arif/Select_by_faculty.java
@@ -1,9 +1,20 @@
+package Select_by_faculty;
+
 import java.sql.*;
 import java.io.*;
 
 public class Select_by_faculty {
     public static void main(String[] args) {
         try {
+              String a = "FTMK"; //--------------> (temporarily) will update base on user selected
+                String faculty = a;
+                //------------------------- header--------------------
+                System.out.println("Faculty : " + faculty);
+                System.out.println("==============\n");
+                System.out.println("----------------------------------------------------------------------------------");
+                System.out.println("||SIGNS CODE||\b\b||DESCRIPTION||\b\b||TOTAL PATIENT||");
+                System.out.println("----------------------------------------------------------------------------------");
+                //------------------------- end header--------------------
             String connectionURL = "jdbc:mysql://127.0.0.1/dump?user=root&password=";
             Connection conn = DriverManager.getConnection(connectionURL);
             String query1 = " select lhr_signs.Signs_code,Signs_desc,count(*) as Bilangan_pelajar from lhr_signs where LOCATION_CODE='FTMK' group by Signs_desc ORDER BY `Signs_desc` ASC";
@@ -11,14 +22,17 @@ public class Select_by_faculty {
             ResultSet rs = st.executeQuery(query1);
             while (rs.next()) {
 
-                System.out.println("Signs_code, Signs_Description , Bilangan_pelajar");
+               
 
                 String signs_code = rs.getString("Signs_code");
                 String Signs_Description = rs.getString("Signs_desc");
                 String Bilangan_pelajar = rs.getString("Bilangan_pelajar");
                
-               
-                System.out.format("%s, %s,%s\n" ,signs_code,Signs_Description,Bilangan_pelajar);
+               System.out.println("||"+ signs_code +"||\b\b||"+ Signs_Description +"||\b\b||"+ Bilangan_pelajar +"||");
+                    //----------------testing
+               System.out.println("----------------------------------------------------------------------------------");
+
+                
             }
             st.close();
         }
@@ -38,7 +52,7 @@ public class Select_by_faculty {
 
                 String Total = rs.getString("Signs_desc");
                 System.out.println("Total number of patient from FTMK:");
-                System.out.format("%s\n",Total);
+                System.out.format("%s\n\n\n",Total);
             }
             st.close();
         }
@@ -47,7 +61,62 @@ public class Select_by_faculty {
             System.err.println(e.getMessage());
         }
 
-        
+        try {
+              String a = "FKEKK"; //--------------> (temporarily) will update base on user selected
+                String faculty = a;
+                //------------------------- header--------------------
+                System.out.println("Faculty : " + faculty);
+                System.out.println("==============\n");
+                System.out.println("----------------------------------------------------------------------------------");
+                System.out.println("||SIGNS CODE||\b\b||DESCRIPTION||\b\b||TOTAL PATIENT||");
+                System.out.println("----------------------------------------------------------------------------------");
+                //------------------------- end header--------------------
+            String connectionURL = "jdbc:mysql://127.0.0.1/dump?user=root&password=";
+            Connection conn = DriverManager.getConnection(connectionURL);
+            String query1 = " select lhr_signs.Signs_code,Signs_desc,count(*) as Bilangan_pelajar from lhr_signs where LOCATION_CODE='FKEKK' group by Signs_desc ORDER BY `Signs_desc` ASC";
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query1);
+            while (rs.next()) {
+
+
+
+                String signs_code = rs.getString("Signs_code");
+                String Signs_Description = rs.getString("Signs_desc");
+                String Bilangan_pelajar = rs.getString("Bilangan_pelajar");
+
+               System.out.println("||"+ signs_code +"||\b\b||"+ Signs_Description +"||\b\b||"+ Bilangan_pelajar +"||");
+                    //----------------testing
+               System.out.println("----------------------------------------------------------------------------------");
+
+
+            }
+            st.close();
+        }
+        catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            String connectionURL = "jdbc:mysql://127.0.0.1/dump?user=root&password=";
+            Connection conn = DriverManager.getConnection(connectionURL);
+
+            String query=" select count(*) Signs_desc from lhr_signs where LOCATION_CODE='FKEKK'";
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+
+                String Total = rs.getString("Signs_desc");
+                System.out.println("Total number of patient from FKEKK:");
+                System.out.format("%s\n\n\n",Total);
+            }
+            st.close();
+        }
+        catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+
     }
 
 
