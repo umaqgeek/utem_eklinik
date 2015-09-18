8731cc9463be2e0b7bf5a6ec37deb89a78dcf18e
amalina <amalina1892@gmail.com>
2015-09-17 22:43:12 -0700

    tukar code kepada int

--- UTeMeClinic/src/utemeclinic/report_by_fac.java
+++ UTeMeClinic/src/utemeclinic/report_by_fac.java
@@ -20,12 +20,11 @@ public class report_by_fac {
                 //------------------------- header--------------------
                 System.out.println("Faculty : " + faculty);
                 System.out.println("==============\n");
-                System.out.println("------------------------------------------------------------------");
-                System.out.println("|| CODE\t\t|| DESCRIPTION\t\t\t|| TOTAL PATIENT||");
-                System.out.println("------------------------------------------------------------------");
+                System.out.println("----------------------------------------------------------------------------------");
+                System.out.println("|| CODE\t\t|| DESCRIPTION\t\t\t\t\t|| TOTAL PATIENT||");
+                System.out.println("----------------------------------------------------------------------------------");
                 //------------------------- end header--------------------
-                
-                //String LC = "FTMK";
+               
                 
                 ArrayList<String> list= new ArrayList<String>();
                 String query = "SELECT icd10_codes.icd10_code, icd10_codes.icd10_chapter, icd10_codes.icd10_block, icd10_codes.icd10_desc, lhr_diagnosis.DiagnosisCd, lhr_diagnosis.LOCATION_CODE, COUNT(*) AS PatientTotal FROM icd10_codes, lhr_diagnosis WHERE icd10_codes.icd10_code = lhr_diagnosis.DiagnosisCd and LOCATION_CODE= 'FTMK' GROUP BY icd10_code, LOCATION_CODE ORDER BY icd10_codes.icd10_code ASC;";
@@ -43,14 +42,22 @@ public class report_by_fac {
                     String PatientTotal = rs.getString("PatientTotal");
 
                     //------------nak buat loop kat sini pecahkan base on chapter/block/code 3 level
-                    System.out.println("|| "+ icd10_code_result +"\t|| "+ icd10_desc +"||\t "+ PatientTotal +"\t||");
+                    System.out.println("|| "+ icd10_code_result +"\t|| "+ icd10_desc +"\t\t||\t "+ PatientTotal +"\t||");
+                    //----------------testing
+                    System.out.println("----------------------------------------------------------------------------------");
+                    System.out.println("code in string:" + icd10_code_result.substring(6));
+                    int foo = Integer.parseInt(icd10_code_result.substring(6));
+                    System.out.println("code in int:" +foo);
+                    //for(foo)
+                    //----------------end testing
+                    System.out.println("----------------------------------------------------------------------------------");
             }
                 
-                System.out.println("------------------------------------------------------------------\n");
+                System.out.println("----------------------------------------------------------------------------------\n");
                 
                 
                 System.out.println("Total Patient by Chapter :");
-                System.out.println("Total Patient by Block :");
+                System.out.println("Total Patient by Block :\n\n");
                 
             st.close();
         }
