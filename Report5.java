0f22e266aae307c5ad46ba4ad28209b699d30eca
adamridhwan275 <adamridhwan275@gmail.com>
2015-09-22 13:30:43 +0800

    Try to implement PreparedStatement to control WHERE sql in java

--- Report5.java
+++ Report5.java
@@ -19,40 +19,51 @@ public class Report5 {
     
     public static void main(String[] args) {
         
+        String[] Faculty={"FTMK","FTK"};//--------------> (temporarily) will update base on user selected
+        int i = 0;
+        int Total = 0;
         
         
         try {
             String connectionURL = "jdbc:mysql://127.0.0.1/servercis?user=root&password=";
             Connection conn = DriverManager.getConnection(connectionURL);
             
-                String a = "FTMK"; //--------------> (temporarily) will update base on user selected
-                String faculty = a;
+                String f = Faculty[i];
+                ArrayList<String> list= new ArrayList<String>();
+                //String query = "SELECT Medication_code, Medication_desc, COUNT(*) as PatientTotal FROM servercis.lhr_medication WHERE LOCATION_CODE='FTMK' group by LOCATION_CODE, Medication_code ;";
+                //String query = "SELECT Medication_code, Medication_desc, COUNT(*) as PatientTotal FROM servercis.lhr_medication WHERE LOCATION_CODE="+ Faculty[i] +" group by LOCATION_CODE, Medication_code ;";
+                String query = "SELECT LOCATION_CODE, Medication_code, Medication_desc, COUNT(Medication_code) as PatientTotal FROM servercis.lhr_medication group by LOCATION_CODE, Medication_code;";
+                
+                //Try to implement PreparedStatement in order to manipulate sql query "WHERE=? to classify each faculty"
+                //PreparedStatement statement = connection.prepareStatement("SELECT LOCATION_CODE, Medication_code, Medication_desc, COUNT(Medication_code) as PatientTotal FROM servercis.lhr_medication WHERE LOCATION_CODE=? group by LOCATION_CODE, Medication_code;");
+                //End try(not working yet)
+                
+                Statement st = conn.createStatement();
+                ResultSet rs = st.executeQuery(query);
+                
+                
+                
                 //------------------------- header--------------------
-                System.out.println("Faculty : " + faculty);
+                System.out.println("Faculty : " + Faculty[i]);
                 System.out.println("==============\n");
                 System.out.println("----------------------------------------------------------------------------------");
                 System.out.println("|| CODE\t\t|| DESCRIPTION\t\t\t\t\t|| TOTAL PATIENT||");
                 System.out.println("----------------------------------------------------------------------------------");
                 //------------------------- end header--------------------
-               
-                
-                ArrayList<String> list= new ArrayList<String>();
-                String query = "SELECT Medication_code, Medication_desc, COUNT(*) as PatientTotal FROM servercis.lhr_medication WHERE LOCATION_CODE='FTMK' group by LOCATION_CODE, Medication_code ;";
-                //String query = "SELECT LOCATION_CODE COUNT(*) as PatientTotal2 FROM servercis.lhr_medication WHERE LOCATION_CODE='FTMK' group by LOCATION_CODE;";
-                Statement st = conn.createStatement();
-                ResultSet rs = st.executeQuery(query);
-                int Total = 0;
                 
                 while (rs.next()) {
                     list.add(rs.getString("Medication_code")); //assign mysql result to list
-                    //String LOCATION_CODE = rs.getString("LOCATION_CODE");
+                    
                     String Medication_code = rs.getString("Medication_code");
                     String Medication_desc = rs.getString("Medication_desc");
                     
                     String PatientTotal = rs.getString("PatientTotal");
 
-                    //------------nak buat loop kat sini pecahkan base on chapter/block/code 3 level
+                    //------------nak buat loop kat sini pecahkan base on code
                     System.out.println("|| "+ Medication_code +"\t|| "+ Medication_desc +"\t\t||\t "+ PatientTotal +"\t||");
+                    
+                    int PT = Integer.parseInt(PatientTotal);
+                    Total += PT;
                     //----------------testing
                     System.out.println("----------------------------------------------------------------------------------");
                     //Total += PatientTotal;
@@ -64,10 +75,11 @@ public class Report5 {
                 
                 System.out.println("----------------------------------------------------------------------------------\n");
                 
-                //list.add(rs.getString("PatientTotal")); //assign mysql result to list
-                //String PatientTotal = rs.getString("PatientTotal");
-                //System.out.println("Total Patient by Chapter :"+PatientTotal+"");
-                //System.out.println("Total Patient by Block :\n\n");
+                
+                System.out.println("Faculty : " + Faculty[i]);
+                System.out.println("Total : " + Total);
+                i++;
+                
                 
             st.close();
         }
