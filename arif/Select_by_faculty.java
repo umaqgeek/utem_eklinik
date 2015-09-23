ee64912cc7ac0d03a6636d3072c20f508e53c1f1
Mohamadarif <butterflyhunter84@gmail.com>
2015-09-23 14:31:56 +0800

    using array to manipulate and loop sql query

--- arif/Select_by_faculty.java
+++ arif/Select_by_faculty.java
@@ -1,80 +1,48 @@
-package Select_by_faculty;
+package select_by_faculty;
 
 import java.sql.*;
-import java.io.*;
+import java.util.ArrayList;
 
-public class Select_by_faculty {
-    public static void main(String[] args) {
+public class Select_by_faculty
+{
+    public static void main(String[]args) throws SQLException
+    {
+        String connectionURL = "jdbc:mysql://127.0.0.1/dump?user=root&password=";
+        Connection conn = DriverManager.getConnection(connectionURL);
+
+        String[] Faculty = new String[7];
+        Faculty[0] = "FTMK";        
+        Faculty[1] = "FKEKK";
+        Faculty[2]= "FTK ";
+        Faculty[3]="FKE";
+        Faculty[4]="FKP";
+        Faculty[5]="FKM";
+        Faculty[6]="FPTT";
+        
+
+        {
+
+for(int i=0; i<Faculty.length; i++)
+{
+    {
         try {
-              String a = "FTMK"; //--------------> (temporarily) will update base on user selected
-                String faculty = a;
+
+                String a= "FTMK";
                 //------------------------- header--------------------
-                System.out.println("Faculty : " + faculty);
+                System.out.println("Faculty : " +Faculty[i]);
                 System.out.println("==============\n");
                 System.out.println("----------------------------------------------------------------------------------");
                 System.out.println("||SIGNS CODE||\b\b||DESCRIPTION||\b\b||TOTAL PATIENT||");
                 System.out.println("----------------------------------------------------------------------------------");
                 //------------------------- end header--------------------
-            String connectionURL = "jdbc:mysql://127.0.0.1/dump?user=root&password=";
-            Connection conn = DriverManager.getConnection(connectionURL);
-            String query1 = " select lhr_signs.Signs_code,Signs_desc,count(*) as Bilangan_pelajar from lhr_signs where LOCATION_CODE='FTMK' group by Signs_desc ORDER BY `Signs_desc` ASC";
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(query1);
-            while (rs.next()) {
 
-               
 
-                String signs_code = rs.getString("Signs_code");
-                String Signs_Description = rs.getString("Signs_desc");
-                String Bilangan_pelajar = rs.getString("Bilangan_pelajar");
-               
-               System.out.println("||"+ signs_code +"||\b\b||"+ Signs_Description +"||\b\b||"+ Bilangan_pelajar +"||");
-                    //----------------testing
-               System.out.println("----------------------------------------------------------------------------------");
 
-                
-            }
-            st.close();
-        }
-        catch (Exception e) {
-            System.err.println("Got an exception! ");
-            System.err.println(e.getMessage());
-        }
+        //String f = Faculty[i];
+        //  ArrayList<String> list= new ArrayList<String>();
 
-        try {
-            String connectionURL = "jdbc:mysql://127.0.0.1/dump?user=root&password=";
-            Connection conn = DriverManager.getConnection(connectionURL);
-
-            String query=" select count(*) Signs_desc from lhr_signs where LOCATION_CODE='FTMK'";
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(query);
-            while (rs.next()) {
-
-                String Total = rs.getString("Signs_desc");
-                System.out.println("Total number of patient from FTMK:");
-                System.out.format("%s\n\n\n",Total);
-            }
-            st.close();
-        }
-        catch (Exception e) {
-            System.err.println("Got an exception! ");
-            System.err.println(e.getMessage());
-        }
-
-        try {
-              String a = "FKEKK"; //--------------> (temporarily) will update base on user selected
-                String faculty = a;
-                //------------------------- header--------------------
-                System.out.println("Faculty : " + faculty);
-                System.out.println("==============\n");
-                System.out.println("----------------------------------------------------------------------------------");
-                System.out.println("||SIGNS CODE||\b\b||DESCRIPTION||\b\b||TOTAL PATIENT||");
-                System.out.println("----------------------------------------------------------------------------------");
-                //------------------------- end header--------------------
-            String connectionURL = "jdbc:mysql://127.0.0.1/dump?user=root&password=";
-            Connection conn = DriverManager.getConnection(connectionURL);
-            String query1 = " select lhr_signs.Signs_code,Signs_desc,count(*) as Bilangan_pelajar from lhr_signs where LOCATION_CODE='FKEKK' group by Signs_desc ORDER BY `Signs_desc` ASC";
-            Statement st = conn.createStatement();
+            String query1 = "select lhr_signs.Signs_code,Signs_desc,count(*) as Bilangan_pelajar from lhr_signs where LOCATION_CODE='" +Faculty[i]+"' group by Signs_desc ORDER BY `Signs_desc` ASC " ;
+            Statement st= conn.createStatement();
             ResultSet rs = st.executeQuery(query1);
             while (rs.next()) {
 
@@ -88,36 +56,49 @@ public class Select_by_faculty {
                     //----------------testing
                System.out.println("----------------------------------------------------------------------------------");
 
-
             }
-            st.close();
-        }
-        catch (Exception e) {
-            System.err.println("Got an exception! ");
-            System.err.println(e.getMessage());
-        }
 
-        try {
-            String connectionURL = "jdbc:mysql://127.0.0.1/dump?user=root&password=";
-            Connection conn = DriverManager.getConnection(connectionURL);
 
-            String query=" select count(*) Signs_desc from lhr_signs where LOCATION_CODE='FKEKK'";
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(query);
-            while (rs.next()) {
+ 
+         try {
 
-                String Total = rs.getString("Signs_desc");
-                System.out.println("Total number of patient from FKEKK:");
+            String query2="select count(*)  Signs_desc from lhr_signs where location_code='" +Faculty[i]+"' group by Location_code" ;
+            Statement st1=conn.createStatement();
+            ResultSet rs2= st1.executeQuery(query2);
+            while(rs2.next()) {
+
+                String Total = rs2.getString("Signs_desc");
+                System.out.println("Total number of patient from :" +Faculty[i]);
                 System.out.format("%s\n\n\n",Total);
+
             }
-            st.close();
+
+         }
+         catch (Exception e)
+         {
+             System.err.println("Got an exception!  ");
+             System.err.println(e.getMessage());
+         }
+
         }
-        catch (Exception e) {
+              catch (Exception e) {
             System.err.println("Got an exception! ");
             System.err.println(e.getMessage());
-        }
+         }
+      
+
+
+            }
+
+
+}
+}
+}
 
     }
 
 
-}
+
+
+
+
